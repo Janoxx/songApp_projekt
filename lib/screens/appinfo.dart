a0697cb4a234af67641a03165f5b73cb6b1cdb07
Jonas <jimbib14@gmail.com>
2021-10-31 13:25:22 +0100

    AppInfo --> beschrieben wie die App funktioniert, änderungen in changelog geschrieben

--- lib/screens/appinfo.dart
+++ lib/screens/appinfo.dart
@@ -1,7 +1,9 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
 class AppInfo extends StatefulWidget {
-  const AppInfo({ Key? key }) : super(key: key);
+  const AppInfo({Key? key}) : super(key: key);
 
   @override
   _AppInfoState createState() => _AppInfoState();
@@ -16,7 +18,51 @@ class _AppInfoState extends State<AppInfo> {
         backgroundColor: Colors.red,
         centerTitle: true,
         title: Text('App Info'),
-        ),
+      ),
+      body: ListView(
+        children: [
+          Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Text("So funktioniert es:",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 28)),
+          ),
+          Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Text(
+                "Die App erleichtert die Übersicht bei Song Releases deiner Lieblingskünstler und Lieblingskünstlerinnen. Indem du Künstlern und Künstlerinnen folgen kannst, werden dir aktuelle Releases auf der Startseite übersichtlich angezeigt."),
+          ),
+          Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Text("Startseite:",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18)),
+          ),
+          Padding(
+            padding: EdgeInsets.fromLTRB(8, 0, 8, 8),
+            child: Text(
+                "Hier siehst du in einer Übsersicht, sortiert nach Tag, Woche oder Monat, die aktuellen Realeases der Künstler und Künstlerinnen denen du folgst."),
+          ),
+          Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Text("Suche:",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18)),
+          ),
+          Padding(
+            padding: EdgeInsets.fromLTRB(8, 0, 8, 8),
+            child: Text(
+                "Hier kannst du nach deinen Lieblingskünstler und Lieblingskünstlerinnen suchen, ihenen folgen und ihre Profile aufrufen."),
+          ),
+          Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Text("Folge ich:",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18)),
+          ),
+          Padding(
+            padding: EdgeInsets.fromLTRB(8, 0, 8, 8),
+            child: Text(
+                "Hier siehst du eine Übersicht deiner gefolgten Künstler und Künstlerinnen. Du kannst gefolgten Künstlern und Künstlerinnen hier wieder entfolgen und auf ihr Profil zugreifen. "),
+          ),
+        ],
+      ),
     );
   }
 }
